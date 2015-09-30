00c2d8e1342300cc29b4175e7325e7e05a86c4d7
Itggot-Lucas-Glimfjord <lucas.glimfjord@itggot.se>
2015-09-30 08:26:57 +0200

    Completed
    Finished the assignment

---
+++ lib/put_to_bed.py
@@ -0,0 +1,6 @@
+__author__ = 'lucas.glimfjord'
+
+def put_to_bed(name1, name2):
+    put_to_bed = "Nighty nighty, {n1} & {n2}!".format(n1=name1, n2=name2)
+    return put_to_bed
+print (put_to_bed(name1="Freddie", name2="Tedd"))
